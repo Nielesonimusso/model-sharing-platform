6dde8c50189dcfabbe2ff5353b6cc06cbbc93f35
Niels Rood <n.rood@tue.nl>
2021-07-05 17:30:30 +0200

    added seed line for ingredient access gateway

--- seed.sh
+++ seed.sh
@@ -4,5 +4,6 @@ docker exec -it $(docker ps -aqf "name=ingredient-service") flask seed-db
 docker exec -it $(docker ps -aqf "name=tomato-saltiness-model-access-gateway") flask seed-db
 docker exec -it $(docker ps -aqf "name=sweet-sourness-model-access-gateway") flask seed-db
 docker exec -it $(docker ps -aqf "name=nutrition-model-access-gateway") flask seed-db
+docker exec -it $(docker ps -aqf "name=ingredient-access-gateway") flask seed-db
 docker exec -it $(docker ps -aqf "name=model-sharing-backend") flask seed-db
 docker exec -it $(docker ps -aqf "name=model-sharing-backend") flask cache-om
